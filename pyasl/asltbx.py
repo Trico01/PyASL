3ab2a044f65043875f9168f74d4773ede1f9891e
Trico <trico010725@gmail.com>
2024-08-02 14:43:01 -0400

    Add util functions

--- pyasl/asltbx.py
+++ pyasl/asltbx.py
@@ -1,30 +1,8 @@
 import os
-import json
 import numpy as np
 import nibabel as nib
 from nipype.interfaces import spm
-
-
-def read_data_description(root: str):
-    description_file = os.path.join(root, "data_description.json")
-
-    try:
-        with open(description_file, "r") as f:
-            data_descrip = json.load(f)
-    except FileNotFoundError:
-        raise ValueError(f"{description_file} not found. Please load data first.")
-    except json.JSONDecodeError:
-        raise ValueError(f"Failed to decode JSON from {description_file}.")
-
-    return data_descrip
-
-
-def load_img(P: str):
-    V = nib.load(P)
-    data = V.get_fdata()
-    data = np.nan_to_num(data)
-
-    return V, data
+from utils.utils import read_data_description, load_img
 
 
 def img_reset_orientation(source_path: str, target_path: str):

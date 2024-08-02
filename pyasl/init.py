26ef4937406386b40e2e2f29142136109f97ccd6
Trico <trico010725@gmail.com>
2024-08-02 14:44:35 -0400

    Update init

--- pyasl/init.py
+++ pyasl/init.py
@@ -1,5 +1,6 @@
 from .data_import import load_data
 from .asl_mricloud import asl_mricloud_pipeline
 from .asltbx import asltbx_pipeline
+from .dlasl import dlasl_pipeline
 
-__all__ = ["load_data", "asl_mricloud_pipeline", "asltbx_pipeline"]
+__all__ = ["load_data", "asl_mricloud_pipeline", "asltbx_pipeline", "dlasl_pipeline"]

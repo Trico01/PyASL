51d51580634b0aa2190437d18ae63989f8f9bb3f
Trico <trico010725@gmail.com>
2024-08-08 13:33:22 -0400

    Add oxford_asl

--- pyasl/init.py
+++ pyasl/init.py
@@ -2,5 +2,12 @@ from .data_import import load_data
 from .asl_mricloud import asl_mricloud_pipeline
 from .asltbx import asltbx_pipeline
 from .dlasl import dlasl_pipeline
+from .oxford_asl import run_oxford_asl
 
-__all__ = ["load_data", "asl_mricloud_pipeline", "asltbx_pipeline", "dlasl_pipeline"]
+__all__ = [
+    "load_data",
+    "asl_mricloud_pipeline",
+    "asltbx_pipeline",
+    "dlasl_pipeline",
+    "run_oxford_asl",
+]

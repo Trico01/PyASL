c9da86a18676836cad9c601c9e7ee9a12116b1bb
Trico <trico010725@gmail.com>
2024-08-23 20:45:00 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -15,8 +15,4 @@ setup(
     ],
     author="Trico",
     author_email="trico010725@gmail.com",
-    description="A library for processing ASL MRI data",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    # url="https://github.com/yourusername/pyasl",
 )
